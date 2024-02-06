5289e130b4d703cfff292079ea2d5f771a066124
prasanna <hariprasannajeni@gmail.com>
2024-02-06 18:46:47 -0500

    Initialized the main function and got inputs from the user.

--- pet/Program.cs
+++ pet/Program.cs
@@ -1,3 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-//gbdghg
+﻿using System;
+using System.Threading;
+
+class Assignment1
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to the Virtual Pet Simulator!");
+
+        // Pet Creation
+        Console.Write("Enter pet type (e.g., cat, dog, rabbit): ");
+        string petType = Console.ReadLine();
+        Console.Write("Enter pet name: ");
+        string petName = Console.ReadLine();
+
+        // Pet Initialization
+        int hunger = 5;
+        int happiness = 5;
+        int health = 5;
+
+        Console.WriteLine($"You have a {petType} named {petName}.");
+
+    }
+}
