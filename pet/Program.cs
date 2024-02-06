d7e75d6a9279b0212ec9cedef61ce39bd325ca80
prasanna <hariprasannajeni@gmail.com>
2024-02-06 18:50:11 -0500

    Initialized the loop for iterative purposes and displaying the pet stats and menu to the user

--- pet/Program.cs
+++ pet/Program.cs
@@ -20,5 +20,21 @@ class Assignment1
 
         Console.WriteLine($"You have a {petType} named {petName}.");
 
-    }
+        //Initializing the loop for repeative actions
+        while (true)
+        {
+            // To display Pet Stats
+
+            Console.WriteLine($"\nPet Stats: Hunger: {hunger}/10 | Happiness: {happiness}/10 | Health: {health}/10");
+
+
+            // User Interaction Menu
+            Console.WriteLine("Choose an action: \n1. Feed\n2. Play\n3. Rest\n4. Exit");
+            //Input option selected from the user
+            string userInput = Console.ReadLine();
+            
+            }
+        }
+
+        }
 }
